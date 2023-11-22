43ef05519be6df0e2883f3a60aae48e47930231a
B216 Instructor <30278906+evan-mitchell93@users.noreply.github.com>
2023-11-22 14:30:29 -0500

    text replaces underscores

--- GUI/my_turtle.py
+++ GUI/my_turtle.py
@@ -1,33 +1,57 @@
 import turtle
 
-secret_word = turtle.textinput("Secret","Enter a word")
+#Constants
+#turtle position for drawing text
+TEXT_POS = (-100,100)
+#turtle position to draw hangman
+IMAGE_POS = (100,150)
+#Flag for won game
+WIN = False
+
+#start players with 0 misses
+misses = 0
+
+#Player enters a secret word
+secret_word = turtle.textinput("Secret","Enter a word").lower()
 num_chars = len(secret_word)
 correct_letters = []
 
-"""t.up()
-t.goto(-50,-100)
-for i in range(num_chars):
-    t.down()
-    t.forward(25)
-    t.up()
-    t.forward(10)"""
-
+#hide the default turtle shape
 turtle.hideturtle()
-starting = turtle.write("_ " * num_chars)
-turtle.write(starting)
-misses = 0
-while misses < 8:
-    guess = turtle.textinput("Guess","Guess a letter in the word")
+turtle.up()
+#move turtle to starting position for text
+turtle.goto(TEXT_POS[0],TEXT_POS[1])
+#display blanks for each character in the secret
+starting = "_ " * num_chars
+#write the _ _ _ _ to the screen
+turtle.write(starting,font=("Arial",20,"normal"))
+
+#will be used to draw the hangman image on bad guesses
+def draw_img(count):
+    #for now do nothing
+    pass
+
+while misses < 8 and WIN == False:
+    guess = turtle.textinput("Guess","Guess a letter in the word").lower()
     if guess in secret_word:
         correct_letters.append(guess)
     else:
         misses += 1
+        draw_img(misses)
     
     output = ""
+    #add a letter or an underscore to the output
     for letter in secret_word:
         if letter in correct_letters:
-            output = output + letter
+            output = output + letter + " "
         else:
-            output = output + "_"
-    #turtle.write(output)
+            output = output + "_ "
+    turtle.clear()
+    turtle.goto(TEXT_POS[0],TEXT_POS[1])
+    turtle.write(output,font=("Arial",20,"normal"))
+
+    #if all letters have been guessed, there will be no "_ " in the output
+    if "_ " not in output:
+        WIN = True
+        turtle.Screen().bgcolor = "blue"
 turtle.mainloop()
